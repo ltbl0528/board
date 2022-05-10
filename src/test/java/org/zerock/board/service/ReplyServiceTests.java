30dfd0b1521e323082e8a46c5a93246c92b531ce
Hyein <ltbl0528@gmail.com>
2022-05-10 23:43:53 +0900

    add test for reply service(getlist)
    add reply controller for reply list

---
+++ src/test/java/org/zerock/board/service/ReplyServiceTests.java
@@ -0,0 +1,26 @@
+package org.zerock.board.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.board.dto.ReplyDTO;
+
+import java.util.List;
+
+@SpringBootTest
+public class ReplyServiceTests {
+
+    @Autowired
+    private ReplyService replyService;
+
+    @Test
+    public void testGetList() {
+
+        //DB에 존재하는 Board 번호
+        Long bno = 100L;
+
+        List<ReplyDTO> replyDTOList = replyService.getList(bno);
+
+        replyDTOList.forEach(replyDTO -> System.out.println(replyDTO));
+    }
+}
